b5195e916cfdee8288b29b5a352cea9a6ea211e1
Aryaan Dilip Vedak <2964823v@student.gla.ac.uk>
2025-11-30 14:15:06 +0000

    build website for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio-website/',
   plugins: [react()],
+  build: {
+    outDir: 'docs', 
+  },
 })
